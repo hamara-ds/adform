03df8221b072eaf4e78497f2f2bdbc0e3709373f
HotelGuthrie <campatguthrie@gmail.com>
2021-12-31 20:05:59 -0600

    harnessed the power of 'stringify' to convert things to JSON for the db to read. Also cleaned up the change handler to make it easier to scale.

--- full-stack/src/App.js
+++ full-stack/src/App.js
@@ -4,38 +4,22 @@ import './App.css';
 class App extends Component {
 
     constructor(props) {
-        console.log('Constructor!!');
         super(props);
         this.state = {
-            item: '',
-            tasks: [],
-            names: []
+            item: "",
+            task: "",
+            name: ""
         };
         this.handleSubmit = this.handleSubmit.bind(this);
-
-        this.taskHandleChange = this.taskHandleChange.bind(this);
-        this.nameHandleChange = this.nameHandleChange.bind(this);
-//        this.nameHandleSubmit = this.nameHandleSubmit.bind(this);
-      //  this.nameHandleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
 
-// this is used to display the database on the screen, unnecessary, therefore i've removed it
-   /* componentDidMount() {
-        console.log('componentDidMount!!');
-        fetch('https://h2hukjth21.execute-api.us-east-1.amazonaws.com/api/tasks')
-        .then(res => res.json())
-        .then(json => {
-            const tasks = json.map(item => item.Task)
-            const names = json.map(item => item.Name);
-            this.setState({ item: tasks, item : names });
-            
-        });
-      }
-*/
+
+
     handleSubmit(e) {
         e.preventDefault();
-        const { item, tasks , names} = this.state;
+         const {  task , name, item} = this.state;
         console.log('handle submit');
         fetch('https://h2hukjth21.execute-api.us-east-1.amazonaws.com/api/tasks', {
             method: 'POST',
@@ -43,49 +27,35 @@ class App extends Component {
               'Accept': 'application/json',
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify({task: item, name: item}, null, '\t')
+            body: JSON.stringify({item:"", "task": task, "name": name}, null, '\t')
         });
-        this.setState({
-            item: '',
-            tasks: [...tasks, item],
-            names: [...names, item]
-        })
-       // this.setState({
-       //     item: '',
-       //     names: [...names, item]
-       // })
-        //e.target.reset();
+     //   console.log(this.State);
+     //   this.setState({
+     //       item: {"task": task, "name": name}
+            
+     //   })
     }
 
-    taskHandleChange(e) {
-        console.log(e.target.value)
+    handleChange(e) {
         this.setState({
-            item: e.target.value
+            [e.target.name]: e.target.value
         });
-    //    e.target.reset();
     }
 
-    nameHandleChange(e) {
-        console.log(e.target.value)
-        this.setState({
-            item: e.target.value
-        });
-     //   e.target.reset();
+   
+    componentDidUpdate() {
+        console.log(this.state);
     }
 
     render() {
-        // you'll need to add names in here if you want to list them
-      //  const { tasks, names } = this.state;
-      //  const todos = tasks.map((value, index) => <li key={ index }>{ value }</li>);
-      //  const totos = names.map((value, index) => <li key={ index }>{ value }</li>);
         return (
             <form onSubmit={ this.handleSubmit } >
                 <div>
                     <label>Task </label>
                     <input 
                     type="text" 
-                   // value={this.state.task}
-                    onChange={ this.taskHandleChange }
+                    onChange={ this.handleChange }
+                    name="task"
                     />
                 </div>
 
@@ -93,11 +63,11 @@ class App extends Component {
                     <label>Name </label>
                     <input 
                     type="text" 
-                    //value={this.state.name}
-                    onChange={ this.nameHandleChange}
+                    onChange={ this.handleChange}
+                    name="name"
                     />
-
                 </div>
+
                 <div>
                 <button type="submit">Submit</button>
                 </div>

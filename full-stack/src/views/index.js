b0ef1d2826916354b6c806bc6768040597fce5dc
Grant Nikseresht <gnikseresht@gmail.com>
2022-02-28 13:40:54 -0600

    Clean repo

--- full-stack/src/views/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-export { default as FormView } from './FormView'
-export { default as IndexView } from './IndexView'
-export { default as ThankYouView } from './ThankYouView'
-
-/* eslint-enable */
